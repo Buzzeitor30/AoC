a7c7ecb7b1bdc733f343a13d3f234107c8cad7a8
Elias Urios Alacreu <eliasurios3001@gmail.com>
2023-12-04 13:08:01 +0100

    black

--- 2023/04/day04.py
+++ 2023/04/day04.py
@@ -1,12 +1,15 @@
 import re
 import numpy as np
+
 with open("input.txt") as file:
     data = file.readlines()
+
 
 def calculate_points(n):
     if n == 0:
         return 0
-    return 2**(n - 1)
+    return 2 ** (n - 1)
+
 
 points = []
 
@@ -21,9 +24,9 @@ for line in data:
     total_correct = winning_numbers & numbers
     points.append(calculate_points((len(total_correct))))
 
-    for j in range(i+1, i + 1 + len(total_correct)):
+    for j in range(i + 1, i + 1 + len(total_correct)):
         scratch_cards[j] += 1 * scratch_cards[i]
     i += 1
 
 print("Total points is:", np.sum(points))
-print("Total of scratch cards is:", np.sum(scratch_cards))               
+print("Total of scratch cards is:", np.sum(scratch_cards))
